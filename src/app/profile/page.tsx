d29a95f217f71acec9cdced0be4521fb85f30ded
Jeni Hoyt <ziahrahl@gmail.com>
2023-08-22 15:39:10 -0400

    add middleware for protected routes

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import axios from "axios";
 import Link from "next/link";
+import { useState } from "react";
 import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
 
 export default function ProfilePage() {
   const router = useRouter();
+  const [data, setData] = useState("");
 
   const handleLogout = async () => {
     try {
@@ -18,16 +20,35 @@ export default function ProfilePage() {
     }
   };
 
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
+
   return (
     <div>
       <h1>Profile</h1>
       <hr />
       <p>Profile page</p>
       <hr />
+      <h2>
+        {data === "" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
       <button
         onClick={handleLogout}
         className="bg-blue-500 text-blue-50">
         Log out
+      </button>
+      <hr />
+      <button
+        onClick={getUserDetails}
+        className="bg-rose-500 text-rose-50">
+        User Details
       </button>
     </div>
   );
